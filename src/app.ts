f541ce76b1a7fb6af1e59566f0c1cd240d21afb1
John Tucker <jtucker@appirio.com>
2019-03-11 14:21:45 -0400

    Test Push

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,6 @@ app.use(
 );
 app.use(bodyParser.json());
 
-app.use('/', (_, res) => res.status(200).send('app is running!'));
+app.use('/', (_, res) => res.status(200).send('app is running 2!'));
 
 export default app;
